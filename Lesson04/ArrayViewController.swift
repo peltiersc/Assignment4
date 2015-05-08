5de42798ed812da88735ba2cfa61a5ca507e6350
peltiersc <punkyhoya@gmail.com>
2015-05-08 10:17:12 -0400

    Nearly completed todos 1 and 2

--- Lesson04/ArrayViewController.swift
+++ Lesson04/ArrayViewController.swift
@@ -8,20 +8,71 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textInput: UITextField!
+    var countries = [String]()
+    let tableViewCell = "tableCell"
+    var newCountry : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textInput.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        textInput.clearsOnBeginEditing = true
+        countries = ["Chile"]
+        
 //        self.dismissViewControllerAnimated(false, completion: nil)
         self.dismissViewControllerAnimated(false, completion: { () -> Void in
             self.view.backgroundColor = UIColor.blueColor()
         })
         self.view.backgroundColor = UIColor.redColor()
+    
+    }
+    
+    @IBAction func countriesCapture(sender: AnyObject) {
+        newCountry = textInput.text
+        countries.append(newCountry)
+        println(countries)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        return true;
+    }
+    
+    // Mark: Table View
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(tableViewCell, forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = countries[indexPath.row]
+        
+        println(countries.count)
+        
+        return cell
+    }
+
         /*
         TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
+        
         TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
         
         */
-    }
 }
 
